2259739f05de5c12e1170dd9a0e522287552c567
BaxtiyorCsharper <08baxti58.com>
2026-01-24 17:13:22 +0500

    update code

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Geometry Guru");
+/* To'g'ri to'rtburchakning yuzini hisoblash
+formulasi: S = a*b       */
+Console.WriteLine("To'g'ri to'rtburchakning tomonlarini kiriting:");
+Console.Write("a=");
+decimal a = Convert.ToDecimal(Console.ReadLine());
+Console.Write("b=");
+decimal b = Convert.ToDecimal(Console.ReadLine());
+decimal S = a * b;
+Console.WriteLine("To'g'ri to'rtburchakning yuzasi:" + S);
